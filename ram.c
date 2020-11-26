fd934ffb8a103faecb90c2996ece1fb64177b4c3
Suraj Magar <suraj.magar0955@cientra.com>
2020-11-26 19:08:17 +0530

    modified2 ram

--- ram.c
+++ ram.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	int a,b;
-	printf("enter two numbers:");
-	scanf("%d %d",&a,&b);
+	int a=1,b=10;
+	//printf("enter two numbers:");
+	//scanf("%d %d",&a,&b);
 	printf("%d\n",a+b);
 	printf("ratna\n");
 }
